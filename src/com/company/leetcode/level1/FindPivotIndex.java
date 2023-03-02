72fec805e2b547c2fe1a03910f23ac27540e09aa
Hung <hungamtp@gmail.com>
2023-03-02 23:43:29 +0700

    hard

--- src/com/company/leetcode/level1/FindPivotIndex.java
+++ src/com/company/leetcode/level1/FindPivotIndex.java
@@ -5,48 +5,21 @@ public class FindPivotIndex {
     public static void main(String[] args) {
         int arr1[] = {1, 7, 3, 6, 5, 6};
         int arr2[] = {1, 2, 3};
-        int arr3[] = {-1, 1, 2};
+        int arr3[] = {2,1,-1};
+        int arr4[] = {0,0,0};
+        System.out.println(pivotIndex(arr1));
         System.out.println(pivotIndex(arr2));
+        System.out.println(pivotIndex(arr3));
     }
 
     public static int pivotIndex(int[] nums) {
-        int numsLength = nums.length;
-        if (numsLength == 0) {
-            return 0;
+        int sum = 0, leftsum = 0;
+        for (int x: nums) sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
         }
-
-        if (numsLength == 1) {
-            return 0;
-        }
-        if (numsLength == 2) {
-            if (nums[0] == nums[1]) {
-                return 1;
-            } else {
-                return -1;
-            }
-        }
-
-        int sumLeft = nums[0];
-        int sumRight = nums[numsLength - 1];
-        int rightIndex = numsLength - 1;
-        int leftIndex = 0;
-
-
-        while (true) {
-            if (sumLeft == sumRight) {
-                if (leftIndex == rightIndex - 2) {
-                    return leftIndex + 1;
-                } else if (leftIndex == rightIndex) {
-                    return leftIndex;
-                }
-            } else if (sumLeft < sumRight) {
-                sumLeft += nums[leftIndex + 1];
-                leftIndex++;
-            } else if (sumLeft > sumRight) {
-                sumRight += nums[rightIndex - 1];
-                rightIndex--;
-            }
-        }
+        return -1;
     }
 }
 
