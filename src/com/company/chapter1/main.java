bfdf391f76f8c664066a4d7097f1b59967ef1708
Hung <hungamtp@gmail.com>
2024-07-25 21:57:13 +0700

    search

--- src/com/company/chapter1/main.java
+++ src/com/company/chapter1/main.java
@@ -3,25 +3,25 @@ package com.company.chapter1;
 public class main {
     public static void main(String[] args) {
         int[] arr = new int[103];
-        for (int i = 0; i <=arr.length-1; i++){
-            arr[i] =i;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            arr[i] = i;
         }
         int x = 55;
-        System.out.println(binarySearch(arr , x));
+        System.out.println(binarySearch(arr, x));
 
     }
 
-    public static boolean binarySearch(int arr[] , int x){
+    public static boolean binarySearch(int arr[], int x) {
         int low = 0;
-        int high = arr.length-1;
-        int mid =0;
-        while (low <= high){
-            mid = (low + high) /2;
+        int high = arr.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
             if (arr[mid] == x) return true;
-            if (arr[mid] > x){
-                high = mid -1;
-            }else {
-                low = mid +1;
+            if (arr[mid] > x) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return false;
