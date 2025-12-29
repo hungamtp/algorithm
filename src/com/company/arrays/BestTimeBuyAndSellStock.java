2723d40ec239412471b2109317037eaef32a7216
hungnb <hungamtp@gmail.com>
2025-12-29 09:01:56 +0700

    max profit

---
+++ src/com/company/arrays/BestTimeBuyAndSellStock.java
@@ -0,0 +1,21 @@
+package com.company.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+public class BestTimeBuyAndSellStock {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4,7,8}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length ; i++) {
+            if (prices[i] > prices[i -1]) {
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
