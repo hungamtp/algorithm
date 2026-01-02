54703ff01657bf19641eb39757c5abe1ec217230
hungnb <hungamtp@gmail.com>
2026-01-02 08:54:02 +0700

    isomorphic using arr istead of map

--- src/com/company/HashTable/Isomorphic.java
+++ src/com/company/HashTable/Isomorphic.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 public class Isomorphic {
     public static void main(String[] args) {
-        System.out.println(isIsomorphic("foo", "bar"));
-        System.out.println(isIsomorphic("add", "egg"));
-        System.out.println(isIsomorphic("badc", "baba"));
+        System.out.println(isIsomorphicUsingCharArr("foo", "bar"));
+        System.out.println(isIsomorphicUsingCharArr("add", "egg"));
+        System.out.println(isIsomorphicUsingCharArr("badc", "baba"));
+
+
+        // a = 97
     }
 
     public static boolean isIsomorphic(String s, String t) {
@@ -29,6 +32,29 @@ public class Isomorphic {
                 }
             } else {
                 mapT.put(t.charAt(i), s.charAt(i));
+            }
+        }
+        return true;
+    }
+    public static boolean isIsomorphicUsingCharArr(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] sArr = new char[256];
+        char[] tArr = new char[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (sArr[s.charAt(i)] != 0) {
+                if (sArr[s.charAt(i)] != t.charAt(i) ) {
+                    return false;
+                }
+            }else{
+                sArr[s.charAt(i)] = t.charAt(i);
+            }
+
+            if (tArr[t.charAt(i)] != 0) {
+                if (tArr[t.charAt(i)] != s.charAt(i) ) {
+                    return false;
+                }
+            }else{
+                tArr[t.charAt(i)] = s.charAt(i);
             }
         }
         return true;
