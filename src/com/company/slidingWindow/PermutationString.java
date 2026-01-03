24031e72b6107ee4afac91fb99f61e550b9f7a83
hungnb <hungamtp@gmail.com>
2026-01-03 19:53:10 +0700

    permutationString

--- src/com/company/slidingWindow/PermutationString.java
+++ src/com/company/slidingWindow/PermutationString.java
@@ -4,34 +4,29 @@ import java.util.Arrays;
 
 public class PermutationString {
     public static void main(String[] args) {
-        System.out.println(checkInclusion("ab", "acab"));
-        System.out.println(checkInclusion("adc", "dcda"));
+
+        System.out.println(checkInclusion("ab", "eidbaooo"));
     }
 
     public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
         int[] arr1 = new int[52];
         int[] arr2 = new int[52];
 
-        int s1Length = s1.length();
-        if(s2.length() < s1Length){
-            return  false;
-        }
-        for (int i = 0; i < s1Length; i++) {
+        int k = s1.length();
+        for (int i = 0; i < s1.length(); i++) {
             arr1[s1.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < s1Length; i++) {
+        for (int i = 0; i < s1.length(); i++) {
             arr2[s2.charAt(i) - 'a']++;
         }
-        if (Arrays.equals(arr1, arr2)) {
-            return true;
-        }
-
-        for (int i = s1Length; i < s2.length(); i++) {
-            arr2[s2.charAt(i) - 'a']++;
-            arr2[s2.charAt(i - s1Length) - 'a']--;
-            if (Arrays.equals(arr1, arr2)) {
-                return true;
-            }
+        if (Arrays.equals(arr1, arr2)) return true;
+        for (int i = 0; i < s2.length() - k; i++) {
+            arr2[s2.charAt(i) - 'a']--;
+            arr2[s2.charAt(i + k) - 'a']++;
+            if (Arrays.equals(arr1, arr2)) return true;
         }
         return false;
     }
