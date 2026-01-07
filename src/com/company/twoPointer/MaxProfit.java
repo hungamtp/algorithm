65b7cc76ca6d1dbeb9b1587fbbe1a7aedfc2b602
hungnb <hungamtp@gmail.com>
2026-01-07 10:43:44 +0700

    max profit

---
+++ src/com/company/twoPointer/MaxProfit.java
@@ -0,0 +1,20 @@
+package com.company.twoPointer;
+
+public class MaxProfit {
+    public static void main(String[] args) {
+//        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+//        System.out.println(maxProfit(new int[]{1,2,11,4,7}));
+        System.out.println(maxProfit(new int[]{1, 2}));
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int maxProfit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - min);
+        }
+        return maxProfit;
+    }
+}
