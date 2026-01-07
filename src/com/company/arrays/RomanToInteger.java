95fe74da8bc3aeff9211f03084a15a20db796a42
hungnb <hungamtp@gmail.com>
2026-01-07 10:43:29 +0700

    roman to integer

---
+++ src/com/company/arrays/RomanToInteger.java
@@ -0,0 +1,35 @@
+package com.company.arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        System.out.println(romanToInt("MCMXCIV"));
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("LVIII"));
+    }
+
+    public static int romanToInt(String s) {
+
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        if (s.isEmpty()) return 0;
+        int result = map.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+}
