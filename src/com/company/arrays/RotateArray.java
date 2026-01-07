e689ce386e5707f1b2ebc38c3a3266a5a4b5fb36
hungnb <hungamtp@gmail.com>
2026-01-07 10:43:17 +0700

    rotate arrray

---
+++ src/com/company/arrays/RotateArray.java
@@ -0,0 +1,26 @@
+package com.company.arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+    }
+
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        for (int i = 0, j = nums.length - k - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        for (int i = nums.length - k, j = nums.length - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
