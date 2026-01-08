56be1d8e3e93028d4de7197da6a57afd6c27343d
hungnb <hungamtp@gmail.com>
2026-01-08 14:20:13 +0700

    Linked List

---
+++ src/com/company/dataStructure/LinkedList.java
@@ -0,0 +1,83 @@
+package com.company.dataStructure;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public Node findMiddleNode() {
+        Node slowP = head;
+        Node fastP = head;
+
+        while (fastP != null && fastP.next != null) {
+            slowP = slowP.next;
+            fastP = fastP.next.next;
+        }
+        return slowP;
+    }
+
+}
+
