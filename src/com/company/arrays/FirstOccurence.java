87be39cf354c21d638acf0c60d5e4a18983acefe
hungnb <hungamtp@gmail.com>
2026-02-02 18:20:32 +0700

    FirstOccurence

--- src/com/company/arrays/FirstOccurence.java
+++ src/com/company/arrays/FirstOccurence.java
@@ -2,7 +2,63 @@ package com.company.arrays;
 
 public class FirstOccurence {
     public static void main(String[] args) {
+        String s2 = "abcxabcdabcdabcy";
+        String s1 = "ababcabab";
+        System.out.println("Is substring: " + isSubstring(s1, s2));
 
+        // A B A B C A B A B D
+        // LPS : 0 0 0 1 2 0 1 2 3 4
+
+    }
+
+    public static boolean isSubstring(String s1, String s2) {
+        if (s1.isEmpty()) return true;
+        if (s2.length() < s1.length()) return false;
+
+        int[] lps = computeLPSArray(s1);
+        int i = 0;
+        int j = 0;
+
+        while (i < s2.length()) {
+            if (s1.charAt(j) == s2.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == s1.length()) {
+                return true; // Match found
+            } else if (i < s2.length() && s1.charAt(j) != s2.charAt(i)) {
+                // Mismatch after j matches
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private static int[] computeLPSArray(String s1) {
+        int[] lps = new int[s1.length()];
+        int len = 0;
+        int i = 1;
+
+        while (i < s1.length()) {
+            if (s1.charAt(i) == s1.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
 
     // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
