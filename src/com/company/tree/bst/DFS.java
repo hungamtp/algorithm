99f4bda80ec0405111b702bf7e2453d830dc9db1
hungnb <hungamtp@gmail.com>
2026-02-12 13:59:01 +0700

    DFS

---
+++ src/com/company/tree/bst/DFS.java
@@ -0,0 +1,51 @@
+package com.company.tree.bst;
+
+public class DFS {
+    public static void main(String[] args) {
+        BST bst = new BST();
+
+        MyTreeNode root = new MyTreeNode(10);
+        bst.root = root;
+        root.left = new MyTreeNode(5);
+        root.right = new MyTreeNode(15);
+        root.left.left = new MyTreeNode(4);
+        root.left.right = new MyTreeNode(6);
+        root.right.right = new MyTreeNode(20);
+        root.right.left = new MyTreeNode(11);
+        System.out.println("DFS Traversal- inorder");
+        inorder(root);
+        System.out.println();
+        System.out.println("DFS Traversal- preorder");
+        preorder(root);
+        System.out.println();
+        System.out.println("DFS Traversal- postorder");
+        postorder(root);
+    }
+
+    public static void inorder(MyTreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.value + " ");
+        inorder(root.right);
+    }
+
+    public static void preorder(MyTreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.value + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void postorder(MyTreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.value + " ");
+    }
+}
