a23c2cc88149479a536e78b1709798be75ef0964
hungnb <hungamtp@gmail.com>
2026-02-15 12:58:58 +0700

    inorder

---
+++ src/com/company/tree/InOrder.java
@@ -0,0 +1,17 @@
+package com.company.tree;
+
+public class InOrder {
+
+
+//        List<Integer> res = new ArrayList<>();
+//        public List<Integer> inorderTraversal(TreeNode root) {
+//            if (root == null){
+//                return res;
+//            }
+//            inorderTraversal(root.left);
+//            res.add(root.val);
+//            inorderTraversal(root.right);
+//            return res;
+//        }
+
+}
